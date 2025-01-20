e026f885ddf6c9b5bd2da29b588ad2fc2f3f9826
Chandsi <111673613+michandgit@users.noreply.github.com>
2025-01-20 22:08:19 +0530

    Update Qbox.js

--- src/Components/Qbox/Qbox.js
+++ src/Components/Qbox/Qbox.js
@@ -7,26 +7,30 @@ const decodeHtml = (html) => {
   return txt.value;
 };
 
-
 const Qbox = (props) => {
-  const { question, correct,index , options ,onSelectAnswer } = props;
- 
+  const { question, index, options, onSelectAnswer } = props;
+
   const handleOptionChange = (e) => {
-    onSelectAnswer(index , e.target.value); // Pass the selected option to the parent
+    onSelectAnswer(index, e.target.value); // Pass the selected option to the parent
   };
+
   const decodedQuestion = decodeHtml(question);
+
   return (
     <div className="box">
-      <h2 id="ques">{index+1}. {decodedQuestion}</h2>
+      <h2 id="ques">{index + 1}. {decodedQuestion}</h2>
       <ul id="ullist">
         {options.map((op, idx) => {
           return (
             <li key={idx}>
-              <input  type="radio" id={`option-${idx}`} 
-              name={`question-${idx}`}
-              value={op}
-              onChange={handleOptionChange} />
-              <label htmlFor={`option-${idx}`}>{op}</label>
+              <input
+                type="radio" // Changed from checkbox to radio
+                id={`option-${index}-${idx}`}
+                name={`question-${index}`} // Same name for all options in the question
+                value={op}
+                onChange={handleOptionChange}
+              />
+              <label htmlFor={`option-${index}-${idx}`}>{op}</label>
             </li>
           );
         })}
